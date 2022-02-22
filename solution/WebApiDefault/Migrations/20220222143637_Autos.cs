a78cc9e127a78c201b75d16a7e4d52c55ffb23a3
Yael Bakker <me@yael.fyi>
2022-02-22 15:45:18 +0100

    Add migration for Auto

---
+++ solution/WebApiDefault/Migrations/20220222143637_Autos.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebApiDefault.Migrations
+{
+    public partial class Autos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Kenteken = table.Column<string>(type: "text", nullable: false),
+                    IsOldtimer = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autos");
+        }
+    }
+}
