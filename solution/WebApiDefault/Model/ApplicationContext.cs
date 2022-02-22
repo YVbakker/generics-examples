03935c58e9428a9344eee9ff57cbae651436ea93
Yael Bakker <me@yael.fyi>
2022-02-22 15:44:57 +0100

    Register auto table as autos

--- solution/WebApiDefault/Model/ApplicationContext.cs
+++ solution/WebApiDefault/Model/ApplicationContext.cs
@@ -5,6 +5,7 @@ namespace WebApiDefault.Model
     public class ApplicationContext : DbContext
     {
         public virtual DbSet<Klant> Klanten { get; set; }
+        public virtual DbSet<Auto> Autos { get; set; }
 
         public ApplicationContext(DbContextOptions options) : base(options)
         {
