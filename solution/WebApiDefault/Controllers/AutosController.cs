008e233eaa4c123c824af6b22b0421121b72b23c
Yael Bakker <me@yael.fyi>
2022-02-22 15:46:06 +0100

    Add Autos controller

---
+++ solution/WebApiDefault/Controllers/AutosController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApiDefault.Services;
+
+namespace WebApiDefault.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AutosController : ControllerBase
+{
+    private readonly IAutosService _autosService;
+
+    public AutosController(IAutosService autosService)
+    {
+        _autosService = autosService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(await _autosService.Get());
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var auto = await _autosService.Get(id);
+        if (auto is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(auto);
+    }
+}
