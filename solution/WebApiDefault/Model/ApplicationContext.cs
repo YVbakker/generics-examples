ca27ba88f074054a6c8cb62e606b72ff58fa1b3d
Yael Bakker <me@yael.fyi>
2022-02-22 15:54:53 +0100

    Add Medewerker model, table as Personeel

--- solution/WebApiDefault/Model/ApplicationContext.cs
+++ solution/WebApiDefault/Model/ApplicationContext.cs
@@ -6,11 +6,12 @@ namespace WebApiDefault.Model
     {
         public virtual DbSet<Klant> Klanten { get; set; }
         public virtual DbSet<Auto> Autos { get; set; }
+        public virtual DbSet<Medewerker> Personeel { get; set; }
 
         public ApplicationContext(DbContextOptions options) : base(options)
         {
-            
+
         }
     }
-    
+
 }
